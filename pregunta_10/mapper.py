aaa907c0251bbfe131377063b4456c2041dded96
CarolinaP04 <cpuertaa@unal.edu.co>
2022-06-19 16:21:26 -0500

    Pregunta_10

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        fields = line.strip().split('\t')
+        number = fields [0]
+        letter =  fields[1]
+
+        sys.stdout.write("{}\t{}\n".format(number,letter))
--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,27 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+
+    dicc = dict()
+
+    for line in sys.stdin:
+
+        number, letters = line.split("\t")
+        letters = letters.strip().split(',')
+
+        for m in letters:
+            if m not in dicc.keys():
+                dicc[m] = number
+            else:
+                dicc[m] = dicc[m] + ',' + number
+
+        tuple_letters = sorted(dicc.items(), key=lambda x: x[0])
+
+    for letter, associated_numbers in tuple_letters:
+        number1 = associated_numbers.split(",")
+        number2 = sorted(number1, key=int)
+        number3 = ",".join(number2)
+
+        sys.stdout.write("{}\t{}\n".format(letter, number3))
